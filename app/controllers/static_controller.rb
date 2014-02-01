4da5bd029ce0b53bbb13e39e01e56effbb4909c3
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-01-31 18:05:01 -0800

    Tried using border-box, didn't seem to do anything. Looking at using flex-box instead.
    Added some logic to a controller, to generate some image urls for display on the page. Will move logic out into helper.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,13 @@
 class StaticController < ApplicationController
 
   def home
+    @images = []
+    5.times do
+      find_image = rand(60000)
+      if Entry.find_by(id: find_image)
+        @images << Entry.find_by(id: find_image).full_image_url
+      end
+    end
   end
 
   def feed
