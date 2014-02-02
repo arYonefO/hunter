45d234f17271af6cde6e22cdc1533c94e26f1504
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-02-02 14:11:11 -0800

    Tweaked CSS to make the holder divs auto width based on the child element.
    Made the home action @images instance variable consistent in it's length.
    
    Shuffled some html between the layouts and the home views

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,7 +2,7 @@ class StaticController < ApplicationController
 
   def home
     @images = []
-    5.times do
+    until @images.count == 2 do
       find_image = rand(60000)
       if Entry.find_by(id: find_image)
         @images << Entry.find_by(id: find_image).full_image_url
