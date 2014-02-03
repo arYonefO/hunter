59a9601e88276c2e7fc01bea08609eb9302cca0f
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-02-02 22:22:05 -0800

    Added doublePica font. Not really exciting to be honest.

--- app/assets/javascripts/pretty-line.js
+++ app/assets/javascripts/pretty-line.js
@@ -22,5 +22,5 @@ $(document).ready(function(){
                 .attr('height', function(d){
                     return d*5;
                 })
-                .attr("fill", "red");
+                .attr("fill", "teal");
           })();
