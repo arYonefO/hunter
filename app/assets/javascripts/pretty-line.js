692c2b0c91d75d116f0bf9b9dff53c78304b9424
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-02-03 08:42:47 -0800

    Tweaked display aspects of the pretty lines and title

--- app/assets/javascripts/pretty-line.js
+++ app/assets/javascripts/pretty-line.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var dataset = [1,2,3,4,3,5,6,7,8,9,5,3,5,7,8,7,5,3,2,1]
+    var dataset = [1,2,3,4,3,5,7,6,8,9,6,3,5,7,8,7,5,3,2,3,2,1,3,4,7,5,6,4,3,2,1,2,1]
     var barPadding = 2
     var w = (window.innerWidth - 30) // IE 6 and below can go away
     var h = 70
@@ -20,7 +20,7 @@ $(document).ready(function(){
                 })
                 .attr('width', w / dataset.length - barPadding)
                 .attr('height', function(d){
-                    return d*5;
+                    return d*5 + 2;
                 })
                 .attr("fill", "teal");
           })();
