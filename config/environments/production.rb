f374219b52dbf41c33f2d790697313f558df263a
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-02-08 18:32:03 -0800

    Attempted to sort out Memcache memory limit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ LecheAsada::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store
+  config.cache_store = :dalli_store, :value_max_bytes => 22176259
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
