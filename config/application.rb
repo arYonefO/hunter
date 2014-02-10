015f690e2aef17655f423137a6c1962894983da7
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-02-09 16:27:25 -0800

    Precompiled the app for Redis' sake

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,6 @@ module LecheAsada
     # config.time_zone = 'Central Time (US & Canada)'
     I18n.enforce_available_locales = false
 
-
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
