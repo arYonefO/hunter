9a4387a0622b44901ea301d54cd9d07d71c75288
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-03-22 16:54:14 -0700

    Tweaked CORS config, to see if adding 'origin' as an origin would settle Safari down.
    Precompiled for deployment
    
    Removed HTTPS from URL, since that isn't currently testable in the dev ENV. Will compare to HTTP shortly.

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
                   "elementType": "labels",
                   "stylers": [ { "visibility": "off" } ] }
               ],
-      data_url: "https://localhost:3002/feed/"
+      data_url: "http://www.graffi.so/feed/"
 
   }
 
