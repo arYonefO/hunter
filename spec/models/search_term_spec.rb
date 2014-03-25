2b249b21ad35b1cf4f20b50f789edbbcde9cb375
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-03-24 21:13:50 -0700

    Created a spec that reasonably covers the creation of search_terms

--- spec/models/search_term_spec.rb
+++ spec/models/search_term_spec.rb
@@ -12,19 +12,13 @@ describe SearchTerm do
   it { should validate_presence_of(:search_term) }
   it { should validate_uniqueness_of(:search_term) }
 
-  # Currently the methods tested below are working as expected in IRB. Need to get the specs to work!
   describe 'receiving an AJAX request to create a new search_term' do
     describe '.update_or_create_search_term' do
-      let!(:berlin_search_term) { 'Berlin, Germany' }
-      let(:berlin_search_term2) { 'Berlin, Germany' }
-      let(:first_time) { SearchTerm.update_or_create_search_term(berlin_search_term) }
-      let(:second_time) { SearchTerm.update_or_create_search_term(berlin_search_term2) }
       it 'should create a new search_term row if the search_term does not exist' do
-        expect(first_time).to change(SearchTerm, :count)
-      end
-
-      it 'should update the existing search_term if it already exists' do
-        expect(second_time).to_not change(SearchTerm, :count)
+        expect{ SearchTerm.update_or_create_search_term('Berlin, Germany') }.to change(SearchTerm, :count)
+        expect{ SearchTerm.update_or_create_search_term('Berlin, Germany') }.to_not change(SearchTerm, :count)
+        expect( SearchTerm.find_by(search_term: 'Berlin, Germany').count ).to eq 2
+        expect( SearchTerm.find_by(search_term: 'Berlin, Germany').when.length ).to eq 2
       end
     end
   end
