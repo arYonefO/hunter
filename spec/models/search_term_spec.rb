b2cb4b2e3178f9cdf6cc34097e78b565952799e2
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-04-15 14:25:46 -0700

    Updated spec for search_term to expect lat and lng fields. These are being required so that the search terms can be used to load the cache with whatever has been looked at before.

--- spec/models/search_term_spec.rb
+++ spec/models/search_term_spec.rb
@@ -8,15 +8,18 @@ describe SearchTerm do
   it { should respond_to(:search_term) }
   it { should respond_to(:count) }
   it { should respond_to(:when) }
+  it { should respond_to(:lat) }
+  it { should respond_to(:lng) }
 
   it { should validate_presence_of(:search_term) }
   it { should validate_uniqueness_of(:search_term) }
 
   describe 'receiving an AJAX request to create a new search_term' do
     describe '.update_or_create_search_term' do
+      let(:params) { { search_term: 'Berlin, Germany', lat: 34.657, lng: 45.678 } }
       it 'should create a new search_term row if the search_term does not exist' do
-        expect{ SearchTerm.update_or_create_search_term('Berlin, Germany') }.to change(SearchTerm, :count)
-        expect{ SearchTerm.update_or_create_search_term('Berlin, Germany') }.to_not change(SearchTerm, :count)
+        expect{ SearchTerm.update_or_create_search_term(params) }.to change(SearchTerm, :count)
+        expect{ SearchTerm.update_or_create_search_term(params) }.to_not change(SearchTerm, :count)
         expect( SearchTerm.find_by(search_term: 'Berlin, Germany').count ).to eq 2
         expect( SearchTerm.find_by(search_term: 'Berlin, Germany').when.length ).to eq 2
       end
