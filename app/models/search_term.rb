9761ec5b2be0c32468c36878118f4171c15ac264
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-04-15 15:15:43 -0700

    Adjusted model and controller to allow for lat and lng to be stored on the search term table

--- app/models/search_term.rb
+++ app/models/search_term.rb
@@ -1,9 +1,10 @@
 class SearchTerm < ActiveRecord::Base
   validates :search_term, presence: true, uniqueness: true
 
-  def self.update_or_create_search_term(search_term)
+  def self.update_or_create_search_term(params)
+    p params[:lat]
     # find_or_create using search_term
-    current_search_term = SearchTerm.find_by(search_term: search_term)
+    current_search_term = SearchTerm.find_by(search_term: params[:search_term])
     time = Time.now.to_s
     if current_search_term
       current_search_term.count += 1
@@ -11,7 +12,13 @@ class SearchTerm < ActiveRecord::Base
       current_search_term.when_will_change!
       current_search_term.save
     else
-      SearchTerm.create('search_term' => search_term, 'when' => [time], 'count' => 1)
+      SearchTerm.create(
+                        'search_term' => params[:search_term],
+                        'when' => [time],
+                        'count' => 1,
+                        'lat' => params[:lat],
+                        'lng' => params[:lng]
+                        )
     end
   end
 end
