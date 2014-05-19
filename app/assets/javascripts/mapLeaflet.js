e3668c50058b2f5e66b3f55719db845987c99605
thomasgwatson <thomasgabriel.watson@gmail.com>
2014-05-18 17:56:20 -0700

    Added two icon constructors and manually tested them out
    Set minZoom to 8.
    
    Expanded create marker layer function to cover two use cases. Conditional statement needs some work
    
    Added test-case for data. So API is working as expected.
    
    Added event listener for zoomend event on the map and created a function that displayed different layers depending on zoom level

--- app/assets/javascripts/mapLeaflet.js
+++ app/assets/javascripts/mapLeaflet.js
@@ -1,11 +1,11 @@
 var mapLeaflet,
     backgroundTiles,
-    myIcon,
-    customLeaflet = {}
+    customLeaflet = {},
+    dataRequest
 
 $(document).ready(function(){
   if ( $('#map-leaflet').length ){
-    mapLeaflet = L.map('map-leaflet').setView([51.505, -0.09], 13);
+    mapLeaflet = L.map('map-leaflet', {minZoom:8}).setView([51.505, -0.09], 13);
 
     backgroundTiles = L.tileLayer('https://{s}.tiles.mapbox.com/v3/examples.map-zr0njcqy/{z}/{x}/{y}.png', {
       attribution: '<a href="http://www.mapbox.com/about/maps/" target="_blank">Terms &amp; Feedback</a>'
@@ -13,38 +13,71 @@ $(document).ready(function(){
 
     backgroundTiles.addTo(mapLeaflet);
 
-    // Icon test-case
-    myIcon = L.divIcon({className: 'leaflet-div-icon'});
-    L.marker([51.505, -0.089], {icon: myIcon}).addTo(mapLeaflet);
+    // Set Icon Constructors
+    customLeaflet.cssDivIcon = L.divIcon({className: 'leaflet-div-icon'});
 
-    // Request data test-case
+    customLeaflet.ThumbnailIcon = L.Icon.extend({
+    options: {
+              iconSize:     [60, 60],
+              iconAnchor:   [5, 15]
+            }
+    })
 
-    customLeaflet.createMarkersLayer = function(data){
-      var convertedPoints = []
+    var exampleThumbURL = "http://origincache-prn.fbcdn.net/10358282_1435179943401252_572496191_s.jpg",
+    testThumbnailIcon = new customLeaflet.ThumbnailIcon({iconUrl: exampleThumbURL})
 
+    L.marker([51.505, -0.089], {icon: testThumbnailIcon}).addTo(mapLeaflet);
+
+    // Boring regular markers
+
+    customLeaflet.createMarkersLayer = function(data, opts){
+      var convertedPoints = [],
+      marker
       for (var i = 0; i < data.length; i++){
         var entry = data[i];
-
-        marker = new L.marker([entry.lat, entry.lng], {icon: myIcon})
+        if (opts['thumbnail'] === true){
+          var thumbnailIcon = new customLeaflet.ThumbnailIcon({iconUrl: entry.thumb})
+          marker = new L.marker([entry.lat, entry.lng], {icon: thumbnailIcon})
+        } else {
+          marker = new L.marker([entry.lat, entry.lng], {icon: customLeaflet.cssDivIcon})
+        }
 
         convertedPoints.push(marker)
       }
-      // console.log(convertedPoints)
-      // So, an issue exists with how the markers are being put into a layer
-      // addLayer may not be working because we are passing it an array of markers, which might not be correct.
-      var markerLayer = L.layerGroup(convertedPoints).addTo(mapLeaflet)
+      return L.layerGroup(convertedPoints)
     }
+
+    // Request data for the test-case (London)
 
     dataRequest = $.ajax({
                     url: "/feed/51/-1",
                   });
 
     dataRequest.done(function(data, status, responseObject){
-      customLeaflet.createMarkersLayer(data)
+      customLeaflet.plainMarkers = customLeaflet.createMarkersLayer(data, {'thumbnail': false})
+      customLeaflet.plainMarkers.addTo(mapLeaflet)
+      customLeaflet.thumbnailMarkers = customLeaflet.createMarkersLayer(data, {'thumbnail': true})
+
+      mapLeaflet.on('zoomend', customLeaflet.onZoomed)
+
     })
 
     dataRequest.fail(function(data, status, responseObject){
       console.log(arguments)
     })
+
+    // Change what layer is displayed based on zoom
+    // Which generates an extra 1500 http requests on display :( Clustering?
+    customLeaflet.onZoomed = function(){
+      console.log(customLeaflet.plainMarkers)
+      console.log(customLeaflet.thumbnailMarkers)
+      if(mapLeaflet.getZoom() >= 18) {
+        mapLeaflet.removeLayer(customLeaflet.plainMarkers);
+        mapLeaflet.addLayer(customLeaflet.thumbnailMarkers);
+      } else {
+        mapLeaflet.removeLayer(customLeaflet.thumbnailMarkers);
+        mapLeaflet.addLayer(customLeaflet.plainMarkers);
+      }
+    }
   }
 })
